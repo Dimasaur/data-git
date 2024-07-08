71d6efd5747317c2e8dbb442fefcbe421496399d
Dima K <@users.noreply.github.com>
2024-07-08 15:21:41 +0200

    Added docstrings, code is 10/10 now

--- today.py
+++ today.py
@@ -1,10 +1,12 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
+
 
 
 def my_name_is():
+    """Added my_name_is function below """
     return "Dimasaur"
 
+
 def my_buddy_is():
+    """Added my_buddy_is function below """
     return "Quentin"
